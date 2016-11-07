01e8d21f5a731dcc57b2536ec2f0c371a30e0741
Andrea.Taras <a.taras@alp.dillingen.de>
2016-11-07 13:19:27 +0100

    added PHP7 constructor

--- js/tc_colourpopup.php
+++ js/tc_colourpopup.php
@@ -48,6 +48,13 @@ class MoodleQuickForm_tccolourpopup extends HTML_QuickForm_text {
      */
     public $_helpbutton = '';
     public $_hiddenLabel = false;
+    
+    /** PHP7 constructor*/
+    
+    public function __construct($elementname = null, $elementlabel = null, $attributes = null, $options = null) {
+    	parent::__construct($elementname, $elementlabel, $attributes);
+    	$this->_type = 'colourtext';
+    }
 
     public function __construct($elementname = null, $elementlabel = null, $attributes = null, $options = null) {
         parent::__construct($elementname, $elementlabel, $attributes);
